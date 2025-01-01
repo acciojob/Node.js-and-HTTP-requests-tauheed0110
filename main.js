9024b61525b5143d13a5f72cad30c0e37d0c5ee2
saif0110 <khanmohdtauheed4@gmail.com>
2025-01-01 12:47:13 +0530

    file changed

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-const https = require('https');
+const http = require('http');
+const fetch = require('node-fetch'); // Ensure `node-fetch` is installed
 
-// TODO: Implement this function
+// Create an HTTPS server
+const server = http.createServer((req, res) => {
+  // Fetch data from external API
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    res.end(data.title);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
